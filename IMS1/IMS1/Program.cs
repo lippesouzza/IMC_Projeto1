eb1b0eda4d0fcaa3587e88bca467c4c27945dc21
lippesouzza <84047056+lippesouzza@users.noreply.github.com>
2022-07-14 20:26:57 -0300

    Lista IMC 1

---
+++ IMS1/IMS1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IMS1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre com peso");
+            float peso = (float)Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entre com sua altura");
+            float altura = (float)Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entre com sua idade");
+            string idade = Console.ReadLine();
+        }
+    }
+}
